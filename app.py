58562f23653efb997640876e982879a1a598994e
RanDom-celll <okayuser75@gmail.com>
2025-02-02 13:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def depression_test():
 
     return render_template("depression_test.html", questions=questions, options=options)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
